b9326301db89d4cd72191e25b54a3ab715db2b81
Caitlin Henry <caitlinhenry.ss@gmail.com>
2022-03-10 15:32:44 -0500

    add more test coverage

--- spec/self_harm_detector_spec.rb
+++ spec/self_harm_detector_spec.rb
@@ -5,11 +5,25 @@ RSpec.describe SelfHarmDetector do
     expect(SelfHarmDetector::VERSION).not_to be nil
   end
 
-  it "detects lack of self harm intent" do 
-    expect(SelfHarmDetector.detect(text: "I really love myself today")).to be 0
+  context "detects lack of self harm intent" do
+    it "detects lack of self harm intent" do 
+      expect(SelfHarmDetector.detect(text: "I really love myself today")).to be 0
+    end
+    it "detects lack of self harm intent when a sentence is similar" do 
+      expect(SelfHarmDetector.detect(text: "I don't want to hurt myself")).to be 0
+    end
   end
-
-  it "detects self harm intent" do 
-    expect(SelfHarmDetector.detect(text: "I want to hurt myself")).to be 1
+  
+  context "detects self harm intent" do
+    it "detects self harm intent" do 
+      expect(SelfHarmDetector.detect(text: "I want to hurt myself")).to be 1
+    end
+    it "detects self harm intent with a score of 2 for two matches" do 
+      expect(SelfHarmDetector.detect(text: "I want to hurt myself because I hate myself")).to be 2
+    end
+    it "detects self harm intent even if punctuation changes" do 
+      expect(SelfHarmDetector.detect(text: "I want to hurt myself!")).to be 1
+    end
+    
   end
 end
